bc0471ea4e76909c836f46e61fc417d96242ea39
daniel-ethridge <56686816+daniel-ethridge@users.noreply.github.com>
2019-12-08 17:56:25 -0500

    Update linear_transform.js

--- linear_transform.js
+++ linear_transform.js
@@ -6,18 +6,12 @@ To work properly, the input list to the function must have at least one coordina
 {
 	var my_list = arrayfromargs(arguments);
 	var d = my_list.pop();
-	var c = my_list.pop();
-	var b = my_list.pop();
 	var a = my_list.pop();
 	var my_transformed_list = new Array(my_list.length);
 	var i;
+	post(my_list + "\n");
 	
-	for (i=0; i < (my_list.length)/2; i++) {
-		
-		my_transformed_list[i] = (my_list[i]/b)-c	
-	};
-	
-	for (i=my_list.length/2; i<my_list.length; i++) {
+	for (i=0; i<my_list.length; i++) {
 		
 		my_transformed_list[i] = a*my_list[i]+d;
 	};	
